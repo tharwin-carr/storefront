eb6afee8186fdebb98070b9937c49c127970dac0
Tharwin Carr <tharwin.carr08@gmail.com>
2022-04-01 23:11:28 -0700

    updated models and removed meta class in customer

--- store/models.py
+++ store/models.py
@@ -71,6 +71,7 @@ class OrderItem(models.Model):
 class Address(models.Model):
     street = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
+    zip_code = models.CharField(max_length=5, default='12345')
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
 
 
